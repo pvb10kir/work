33c3e8e4cafc386fa3e64958322540ca45deb919
pvb10kir <sehr73@chmail.ir>
2018-01-13 02:15:22 +0330

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -64,7 +64,7 @@ local function do_keyboard_act()
     local keyboard = {}
     keyboard.inline_keyboard = {
     	{
-    		{text = 'انتقال به درگاه🌐', url = 'hamsargram.tk/TejaratBank/Payment.html'},
+    		{text = 'انتقال به درگاه🌐', url = 'hamsarg.tk/TejaratBank/Payment.html'},
 	    },
 	{
 	        		{text = '🔙بازگشت', callback_data = '!next'},
