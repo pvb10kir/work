cf3926e0328047ef94e1bc9d9b1687c1a5a9858c
pvb10kir <sehr73@chmail.ir>
2018-01-20 12:50:31 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
-	bot_api_key = '482928362:AAFtj-o1x7L2svZST0wdjnWaI2Ig2OANYpA',
+	bot_api_key = '374809367:AAFqVUSujOeHnM1wnxN_t4oNTQbXPOGI0pg',
 	admin = 255317894, 
 	channel = '@sphero_ch', --channel username with the '@'
 
