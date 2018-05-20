27969143a5b3bc3be9591d4c981b559851e9d09c
pvb10kir <sehr73@chmail.ir>
2018-05-20 22:16:21 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -81,7 +81,7 @@ local action = function(msg, blocks, ln)
             local keyboard = do_keyboard_private()
             api.sendKeyboard(msg.from.id, message, keyboard, true)
             end
-api.sendMessage(msg.from.id, 'سلام برای دریافت اخبار جدید ربات و دیدن نمونه ها و کدهای صیغه جدید در کانال ربات جوین شین:\n[برای جوین شدن روی اینجا کلیک کنید](https://t.me/joinchat/AAAAAFJeVnS06dhvmocc3w)', true)
+api.sendMessage(msg.from.id, 'سلام برای دریافت اخبار جدید ربات و دیدن نمونه ها و کدهای صیغه جدید در کانال ربات جوین شین:\n[برای جوین شدن روی اینجا کلیک کنید](https://t.me/joinchat/AAAAAEKbz4bDRS8GvQyWvQ)', true)
     end
 if blocks[1] == 'broadcast' and blocks[2] and is_admin(msg) then
     local gps = db:scard('bot:bc3') or 0
